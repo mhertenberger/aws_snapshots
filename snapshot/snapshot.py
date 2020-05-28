98819f344a24ed563df00b1d9aa5b6e6e413b08f
Manfred Hertenberger <mhertenberger@Manfreds-MacBook-Pro-2.local>
2020-05-28 11:50:28 +0800

    Added click for command line options

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -1,13 +1,21 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name="snapshot")
 ec2 = session.resource("ec2")
 
+@click.command()
 def list_instances():
+    "List AWS EC2 instances"
     for i in ec2.instances.all():
-        print(i)
+        print(",".join((
+            i.id,
+            i.instance_type,
+            i.placement["AvailabilityZone"],
+            i.state["Name"],
+            i.public_dns_name)))
+    return
+
 
 if __name__ == "__main__":
     list_instances()
-
-    
