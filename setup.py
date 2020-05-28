3f72ef57d334c93aaea7df81525d5abd4003ef99
Manfred Hertenberger <mhertenberger@Manfreds-MacBook-Pro-2.local>
2020-05-28 15:50:05 +0800

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "snapshot",
+    version = "0.1",
+    author = "Manfred Hertenberger",
+    author_email = "mhertenberger@gmail.com",
+    description = "A tool to manage AWS EC2 snapshots.",
+    license = "GPLv3+",
+    packages = ["snapshot"],
+    url = "https://github.com/mhertenberger/aws_snapshots",
+    install_requires = [
+        "click",
+        "boto3"
+    ],
+    entry_points = """
+        [console_scripts]
+        snapshot = snapshot.snapshot:click
+    """,
+)
