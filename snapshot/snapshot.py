565e1167ebdd6101e6677c40e5ee954804741adc
Manfred Hertenberger <mhertenberger@Manfreds-MacBook-Pro-2.local>
2020-05-28 11:29:31 +0800

    Reformated snapshot.py to be ready for additional functions

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -1,8 +1,13 @@
 import boto3
 
-if __name__ == "__main__":
-    session = boto3.Session(profile_name="snapshot")
-    ec2 = session.resource("ec2")
+session = boto3.Session(profile_name="snapshot")
+ec2 = session.resource("ec2")
 
+def list_instances():
     for i in ec2.instances.all():
         print(i)
+
+if __name__ == "__main__":
+    list_instances()
+
+    
